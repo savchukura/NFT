5746049c6b4eebb0f1732f556f0db0f5bd1ffd1e
savchukura <savcukura@gmail.com>
2024-02-03 19:15:13 +0200

    Adding tests for web

--- conftest.py
+++ conftest.py
@@ -33,7 +33,6 @@ def auth_token():
     id_token = requests.post(
         url='https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDLejcXq2tpapC9Gk93O1aUv18LIM1xKRY',
         data=json.dumps(data))
-    #print(id_token.json()['idToken'])
     data = {
         'idToken': id_token.json()['idToken'],
         'fingerprint': 'd1c8f9185507f41eef029f10f2fd939acc2d4a61',
@@ -44,7 +43,6 @@ def auth_token():
         requests.post(url="https://liquid-acre.zpoken.dev/api/v1/auth", json=data, headers=headers).text)
 
     token = get_token['accessToken']['access_token']
-    #print(token)
     return token
 
 
